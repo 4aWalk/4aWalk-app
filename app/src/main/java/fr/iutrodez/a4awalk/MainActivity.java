a3774717a483816be283b90fe461cc74ce512da4
ALEXANDRE BROUZES <alexandre.brouzes@iut.local>
2025-12-03 17:27:58 +0100

    sauvegarde branche
    Premier push branch

--- app/src/main/java/fr/iutrodez/a4awalk/MainActivity.java
+++ app/src/main/java/fr/iutrodez/a4awalk/MainActivity.java
@@ -3,12 +3,28 @@ package fr.iutrodez.a4awalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.header);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Remplace 'votre_menu' par le nom réel de ton fichier XML de menu
+        getMenuInflater().inflate(R.menu.header_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
+    }
+
+
 }
