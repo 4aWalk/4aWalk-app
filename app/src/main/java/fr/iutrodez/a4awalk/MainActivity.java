2fb0248571d57dc3e7d6ba9f0cfb8774828944ec
TONY LAPEYRE <tony.lapeyre@iut.local>
2025-12-04 13:54:11 +0100

    application qui marche

--- app/src/main/java/fr/iutrodez/a4awalk/MainActivity.java
+++ app/src/main/java/fr/iutrodez/a4awalk/MainActivity.java
@@ -3,28 +3,12 @@ package fr.iutrodez.a4awalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.header);
+        setContentView(R.layout.main_activity);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Remplace 'votre_menu' par le nom réel de ton fichier XML de menu
-        getMenuInflater().inflate(R.menu.header_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return true;
-    }
-
-
 }
