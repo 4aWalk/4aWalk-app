e68220dd0f3d01991df6a343f523b79fc9e0cc19
CrabeNeo <neo.becogne@iut-rodez.fr>
2026-01-27 16:58:00 +0100

    fix probleme retour et header

--- app/src/main/java/fr/iutrodez/a4awalk/GestionCompte/UpdateProfilActivity.java
+++ app/src/main/java/fr/iutrodez/a4awalk/GestionCompte/UpdateProfilActivity.java
@@ -1,18 +1,45 @@
 package fr.iutrodez.a4awalk.GestionCompte;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 
 import fr.iutrodez.a4awalk.R;
 
 public class UpdateProfilActivity extends AppCompatActivity {
 
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.modification_compte); // Assurez-vous que c'est le bon nom de votre layout
+        setContentView(R.layout.modification_compte);
+
+        toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setDisplayShowTitleEnabled(false);
+            }
+        }
+
+        // Bouton retour
+        Button btnRetour = findViewById(R.id.btn_retour);
+        btnRetour.setOnClickListener(v -> {
+            Intent intent = new Intent(UpdateProfilActivity.this, ProfilActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_header, menu);
+        return true;
     }
 }
